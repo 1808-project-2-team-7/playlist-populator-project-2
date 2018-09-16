9ea7b48f1a5a6a310075e1182a5af06de1c6b1eb
Adrien Lindner <lindner.54@osu.edu>
2018-09-16 14:45:21 -0400

    Added some tests for sign in actions

--- client/src/actions/sign-in/sign-in.actions.tsx
+++ client/src/actions/sign-in/sign-in.actions.tsx
@@ -1,4 +1,6 @@
+import history from '../../history';
 import { signInTypes } from "./sign-in.types";
+import { environment } from '../../environment';
 
 export const updatePassword = (password: string) => {
   return {
@@ -27,3 +29,59 @@ export const updateError = (errorMessage: string) => {
   }
 }
 
+export const login = (e: React.FormEvent<HTMLFormElement>, credentials: any) => {
+  return (dispatch: any) => {
+    e.preventDefault();
+    return fetch(`${environment.context}users/login`, {
+      body: JSON.stringify(credentials),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+    })
+      .then(resp => {
+        if (resp.status === 200) {
+          return resp.json();
+        } else {
+          return resp;
+        }
+      })
+      .then(resp => {
+        switch (resp.status) {
+          case 401:
+            dispatch({
+              payload: {
+                currentUser: null,
+                errorMessage: 'Invalid username or password'
+              },
+              type: signInTypes.LOGIN
+            });
+            break;
+          case undefined:
+            sessionStorage.setItem('currentUser', JSON.stringify(resp));
+            dispatch({
+              payload: {
+                currentUser: resp,
+                errorMessage: ''
+              },
+              type: signInTypes.LOGIN
+            });
+            history.push('/home');
+            break;
+          default:
+            throw new Error("Failed to login at this time");
+        }
+      })
+      .catch(err => {
+        dispatch({
+          payload: {
+            currentUser: null,
+            errorMessage: 'Failed to login at this time'
+          },
+          type: signInTypes.LOGIN
+        });
+        console.log(err);
+      });
+  }
+}
