861a05863af4167b5e5b05f14cce96632665de2c
Adrien Lindner <lindner.54@osu.edu>
2018-09-18 11:49:46 -0400

    Implemented register reducer and added tests (All tests pass)

--- client/src/reducers/index.tsx
+++ client/src/reducers/index.tsx
@@ -1,24 +1,37 @@
 import { combineReducers } from "redux";
 import { signInReducer } from "./sign-in.reducer"
+import { registerReducer } from "./register.reducer"
 
 export interface ISignInState {
-    credentials: {
-      password: string,
-      username: string
-    },
-    currentUser: any,
-    errorMessage: string
-  }
+  credentials: {
+    password: string,
+    username: string
+  },
+  currentUser: any,
+  errorMessage: string
+}
 
-  export interface IState {
-    signIn: ISignInState
-  }
+export interface IRegisterState {
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  errorMessage: string,
+  currentUser: any
+}
+
+export interface IState {
+  register: IRegisterState,
+  signIn: ISignInState
+}
 
 const reducer = combineReducers<IState>({
-    signIn: signInReducer
-  })
+  register: registerReducer,
+  signIn: signInReducer
+})
 
-  export const state = (newState: any, action: any) => {
+export const state = (newState: any, action: any) => {
 
-    return reducer(newState, action)
-  }
+  return reducer(newState, action)
+}
---
+++ client/src/reducers/register.reducer.tsx
@@ -0,0 +1,59 @@
+import { registerTypes } from "../actions/register/register.types";
+import { IRegisterState } from ".";
+
+export const initialState: IRegisterState = {
+  currentUser: null,
+  email: '',
+  errorMessage: '',
+  firstName: '',
+  lastName: '',
+  password: '',
+  username: ''
+}
+
+export const registerReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case registerTypes.UPDATE_USERNAME:
+      return {
+        ...state,
+        username: action.payload.username
+      }
+    case registerTypes.UPDATE_PASSWORD:
+      return {
+        ...state,
+        password: action.payload.password
+      }
+    case registerTypes.UPDATE_FIRST_NAME:
+      return {
+        ...state,
+        firstName: action.payload.firstName
+      }
+    case registerTypes.UPDATE_LAST_NAME:
+      return {
+        ...state,
+        lastName: action.payload.lastName
+      }
+    case registerTypes.UPDATE_EMAIL:
+      return {
+        ...state,
+        email: action.payload.email
+      }
+    case registerTypes.REGISTER:
+      const errorMessage = action.payload.errorMessage
+      const newState = {
+        ...state,
+        currentUser: action.payload.currentUser,
+        errorMessage
+      }
+      if (!errorMessage) {
+        newState.email = '';
+        newState.firstName = '';
+        newState.lastName = '';
+        newState.password = '';
+        newState.username = '';
+      }
+      return newState;
+  }
+
+  return state;
+}
