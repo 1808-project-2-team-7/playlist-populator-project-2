87ecdb9eaacb5b1e4d2e41c02e6e6ae15ba09b20
haus20xx <kennysmod@gmail.com>
2018-09-20 14:55:30 -0400

    made it the right create playlist

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import './include/bootstrap'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomeComponent from './components/home/home.component';
 import ProfileComponent from './components/user/profile.component';
-import CreatePlaylistComponent from './components/playlist/create-playlist.component'
 import PlaylistComponent from './components/playlist/playlist.component'
 import SignInComponent from './components/sign-in/sign-in.component';
 import RegisterComponent from './components/register/register.component';
@@ -14,6 +13,7 @@ import { AppNav } from './components/navigation/app-nav';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { User } from './models/User';
+import createPlaylistComponent from './components/create-playlist/create-playlist.component';
 
 export const getCurrentUser = () => {
   const currentUser = store.getState().currentUser;
@@ -38,7 +38,7 @@ class App extends React.Component {
                 <Route path="/profile" component={ProfileComponent} />
                 <Route path="/sign-in" component={SignInComponent} />
                 <Route path="/register" component={RegisterComponent} />
-                <Route path="/playlists/new" component={CreatePlaylistComponent} />
+                <Route path="/playlists/new" component={createPlaylistComponent} />
                 <Route path="/playlists/:id" component={PlaylistComponent} />
                 <Route path="/home" component={HomeComponent} />
                 <Route component={HomeComponent} />
