bac6f2c2355ac6ea28c5f94ce068cab51d7c2509
Adrien Lindner <lindner.54@osu.edu>
2018-09-19 20:54:59 -0400

    Changed port fetching from to 9001

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://ec2-18-188-226-1.us-east-2.compute.amazonaws.com:3000/'
+  context: 'http://ec2-18-188-226-1.us-east-2.compute.amazonaws.com:9001/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
