3e58a916935523bffed7ba2bfc5c475699e5ef8a
Adrien Lindner <lindner.54@osu.edu>
2018-09-21 15:40:32 -0400

    Changed ec2 url

--- client/src/environment.ts
+++ client/src/environment.ts
@@ -4,7 +4,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://ec2-52-14-119-221.us-east-2.compute.amazonaws.com:9002/'
+  context: 'http://ec2-18-224-1-78.us-east-2.compute.amazonaws.com:9002/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
--- client/src/test/environment.test.tsx
+++ client/src/test/environment.test.tsx
@@ -3,15 +3,15 @@ describe('environment url', () => {
         jest.resetModules();
     });
     afterEach(() => delete process.env.NODE_ENV);
-    it('gives localhost with port 9001 on when NODE_ENV environment variable is not production', () => {
+    it('gives localhost with port 9002 on when NODE_ENV environment variable is not production', () => {
         process.env.NODE_ENV = 'development';
         const environment = require('../../src/environment').environment;
-        expect(environment.context).toEqual('http://localhost:9001/');
+        expect(environment.context).toEqual('http://localhost:9002/');
     });
 
     it('gives production url', () => {
         process.env.NODE_ENV = 'production';
         const environment = require('../../src/environment').environment;
-        expect(environment.context).toEqual('http://ec2-52-14-119-221.us-east-2.compute.amazonaws.com:9002/');
+        expect(environment.context).toEqual('http://ec2-18-224-1-78.us-east-2.compute.amazonaws.com:9002/');
     });
 });
