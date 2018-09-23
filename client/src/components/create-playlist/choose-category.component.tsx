383dfe9eca49e4c2a9b56b8c3080851f7ee022c0
Adrien Lindner <lindner.54@osu.edu>
2018-09-23 00:12:16 -0400

    Category card image paths are now fetched from database instead of hardcoded

--- client/src/components/create-playlist/choose-category.component.tsx
+++ client/src/components/create-playlist/choose-category.component.tsx
@@ -1,72 +1,52 @@
 import * as React from "react";
-import { ICreatePlaylistState, IState } from "../../reducers";
 import { connect } from "react-redux";
-import CategoryCardComponent from "./category-card.component";
+import { getCategories } from "../../App";
 import { Category } from "../../models/Category";
-import { environment } from "../../environment";
+import { ICreatePlaylistState, IState } from "../../reducers";
+import CategoryCardComponent from "./category-card.component";
 
 interface ICategoryState {
-  categories: Category[],
-  imageTitles: string[]
+  categories: Category[]
 }
 
 export class ChooseCategoryComponent extends React.Component<ICreatePlaylistState, ICategoryState> {
 
-  public constructor(props: any){
+  public constructor(props: any) {
     super(props);
-    this.state={
-      categories: [new Category(1, 'Productivity'),
-                  new Category(2, 'Travel'),
-                  new Category(3, 'Going Out'),
-                  new Category(4, 'Unwind'),
-                  new Category(5, 'Exercise'),
-                  new Category(6, 'Melancholy'),
-                  new Category(7, 'Upbeat'),
-                  new Category(8, 'Throwback'),
-                  new Category(9, 'Moody')],
-      imageTitles: ['productivity', 'travel', 'goingOut', 'unwind', 'exercise', 'melancholy',
-                    'upbeat', 'throwback', 'moody']
-    }
+    this.state = { categories: [] }
   }
 
-  public componentDidMount(){
-    fetch(`${environment.context}categories`)
-    .then(resp => resp.json())
-    .then(returnedCategories => {
-      this.setState({
-        categories: returnedCategories
-      })
-    })
-    .catch(error => {
-      console.log(error)
+  public componentDidMount() {
+    this.setState({
+      categories: getCategories()
     })
   }
 
   public render() {
+    const categories = this.state.categories;
+    const images = categories.map(c => c.imagePath);
     return (
       <div className="container">
-      {
-        this.state.categories.map((category : Category, index: number) => {
-          const categories=this.state.categories;
-          const images=this.state.imageTitles;
-          if(index%3===0 && index-2 < this.state.categories.length){
-            return (
-              <div key={category.id} className="row">
+        {
+          categories.map((category: Category, index: number) => {
+            if (index % 3 === 0 && index - 2 < this.state.categories.length) {
+              return (
+                <div key={category.id} className="row">
                   <div className="col-sm">
-                  <CategoryCardComponent imagePath={images[index]} category={categories[index]}/>
+                    <CategoryCardComponent imagePath={images[index]} category={categories[index]} />
                   </div>
                   <div className="col-sm">
-                  <CategoryCardComponent imagePath={images[index+1]} category={categories[index+1]}/>
+                    <CategoryCardComponent imagePath={images[index + 1]} category={categories[index + 1]} />
                   </div>
                   <div className="col-sm">
-                  <CategoryCardComponent imagePath={images[index+2]} category={categories[index+2]}/>
+                    <CategoryCardComponent imagePath={images[index + 2]} category={categories[index + 2]} />
                   </div>
-              </div>
-            )
-          }
-          return;
-        })
-      }
+                </div>
+              )
+            }
+            return;
+          })
+        }
       </div>
     );
   }
