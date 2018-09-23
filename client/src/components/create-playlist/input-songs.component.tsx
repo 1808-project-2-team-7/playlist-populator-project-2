c015a5632b01fc04615089fb0b82d38b740ab87f
Adrien Lindner <lindner.54@osu.edu>
2018-09-23 03:09:46 -0400

    Added autofocus to first input on input songs component

--- client/src/components/create-playlist/input-songs.component.tsx
+++ client/src/components/create-playlist/input-songs.component.tsx
@@ -7,72 +7,72 @@ import { Playlist } from "../../models/Playlist";
 import { Button } from "reactstrap";
 
 interface IProps extends ICreatePlaylistState {
-    addInputToPlaylist: (songInput: string, artistInput: string, accessToken: string) => any;
-    clearSuggestedSongs: () => any;
-    getSongsFromDatabase: (playlist: Playlist, spotifyApiSongs: Song[]) => any;
-    getSongsFromSpotifyApi: (songs: Song[], accessToken: string) => any;
-    getSuggestedSongs: (song: Song, accessToken: string) => any;
-    updateArtistInput: (artistInput: string) => any;
-    updateSongInput: (songInput: string) => any;
+  addInputToPlaylist: (songInput: string, artistInput: string, accessToken: string) => any;
+  clearSuggestedSongs: () => any;
+  getSongsFromDatabase: (playlist: Playlist, spotifyApiSongs: Song[]) => any;
+  getSongsFromSpotifyApi: (songs: Song[], accessToken: string) => any;
+  getSuggestedSongs: (song: Song, accessToken: string) => any;
+  updateArtistInput: (artistInput: string) => any;
+  updateSongInput: (songInput: string) => any;
 }
 
 export class InputSongsComponent extends React.Component<IProps, IState> {
 
-  public constructor(props: any){
+  public constructor(props: any) {
     super(props);
   }
 
-  public updateArtist= (e: any) => {
-      this.props.updateArtistInput(e.target.value);
+  public updateArtist = (e: any) => {
+    this.props.updateArtistInput(e.target.value);
   }
 
-  public updateSong= (e: any) => {
-      this.props.updateSongInput(e.target.value);
+  public updateSong = (e: any) => {
+    this.props.updateSongInput(e.target.value);
   }
 
-  public addToPlaylist= async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const song= await this.props.addInputToPlaylist(this.props.songInput, this.props.artistInput, this.props.accessToken);
-      this.props.clearSuggestedSongs();
-      song && this.props.getSuggestedSongs(song, this.props.accessToken);
-  }
-
-  public populate= async (e: React.FormEvent<HTMLFormElement>) => {
+  public addToPlaylist = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(this.props.playlist.songs.length < 5){
-      const spotifyApiSongs=await this.props.getSongsFromSpotifyApi(this.props.playlist.songs, this.props.accessToken);
+    const song = await this.props.addInputToPlaylist(this.props.songInput, this.props.artistInput, this.props.accessToken);
+    this.props.clearSuggestedSongs();
+    song && this.props.getSuggestedSongs(song, this.props.accessToken);
+  }
+
+  public populate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (this.props.playlist.songs.length < 5) {
+      const spotifyApiSongs = await this.props.getSongsFromSpotifyApi(this.props.playlist.songs, this.props.accessToken);
       this.props.getSongsFromDatabase(this.props.playlist, spotifyApiSongs);
     }
-    else{
+    else {
       // update message
     }
   }
 
-  public showPopulateButton= () => {
-    if(this.props.playlist.songs.length >= 3){
+  public showPopulateButton = () => {
+    if (this.props.playlist.songs.length >= 3) {
       return (
         <form onSubmit={this.populate}>
-            <div>
-                <Button className="submit-button" type="submit"> Populate </Button>
-            </div>
+          <div>
+            <Button className="submit-button" type="submit"> Populate </Button>
+          </div>
         </form>
       )
     }
     return;
   }
-  
+
   public render() {
     return (
       <div id="create-playlist" className="container">
-          <form onSubmit={this.addToPlaylist}>
-            <div className="form-group">
-                <label>Enter Song Title: </label>
-                <input onChange={this.updateSong} type="text" className="form-control" id="formGroupExampleInput" placeholder="Song Title" />
-                <label>Enter Artist: </label>
-                <input onChange={this.updateArtist} type="text" className="form-control" id="formGroupExampleInput" placeholder="Artist Name" />
-                <br />
-                <Button className="submit-button" type="submit"> Submit </Button>
-            </div>
+        <form onSubmit={this.addToPlaylist}>
+          <div className="form-group">
+            <label>Enter Song Title: </label>
+            <input onChange={this.updateSong} type="text" className="form-control" id="formGroupExampleInput" placeholder="Song Title" autoFocus />
+            <label>Enter Artist: </label>
+            <input onChange={this.updateArtist} type="text" className="form-control" id="formGroupExampleInput" placeholder="Artist Name" />
+            <br />
+            <Button className="submit-button" type="submit"> Submit </Button>
+          </div>
         </form>
         {this.showPopulateButton()}
       </div>
@@ -81,12 +81,12 @@ export class InputSongsComponent extends React.Component<IProps, IState> {
 }
 const mapStateToProps = (state: IState) => (state.createPlaylist);
 const mapDispatchToProps = {
-    addInputToPlaylist: createPlaylistActions.addInputToPlaylist,
-    clearSuggestedSongs: createPlaylistActions.clearSuggestedSongs,
-    getSongsFromDatabase: createPlaylistActions.getSongsFromDatabase,
-    getSongsFromSpotifyApi: createPlaylistActions.getSongsFromSpotifyApi,
-    getSuggestedSongs: createPlaylistActions.getSuggestedSongs,
-    updateArtistInput: createPlaylistActions.updateArtistInput,
-    updateSongInput: createPlaylistActions.updateSongInput
+  addInputToPlaylist: createPlaylistActions.addInputToPlaylist,
+  clearSuggestedSongs: createPlaylistActions.clearSuggestedSongs,
+  getSongsFromDatabase: createPlaylistActions.getSongsFromDatabase,
+  getSongsFromSpotifyApi: createPlaylistActions.getSongsFromSpotifyApi,
+  getSuggestedSongs: createPlaylistActions.getSuggestedSongs,
+  updateArtistInput: createPlaylistActions.updateArtistInput,
+  updateSongInput: createPlaylistActions.updateSongInput
 }
 export default connect(mapStateToProps, mapDispatchToProps)(InputSongsComponent);
