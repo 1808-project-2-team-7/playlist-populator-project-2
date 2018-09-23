6b9eb95518160a2d560299ab7b3faf920bf5e2cc
Adrien Lindner <lindner.54@osu.edu>
2018-09-23 02:56:10 -0400

    All categories are now selected after logout instead of none

--- client/src/actions/current-user/current-user.actions.tsx
+++ client/src/actions/current-user/current-user.actions.tsx
@@ -1,10 +1,12 @@
 import history from '../../history';
 import { currentUserTypes } from "./current-user.types";
+import { getCategories } from '../../App';
 
 export const logout = () => {
   history.push('/');
   return {
     payload: {
+      categories: getCategories(),
       currentUser: null
     },
     type: currentUserTypes.LOGOUT
